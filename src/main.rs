e4354296749ff053c686d7ad5ebd010df0f304d2
Ice748 <vinogradovsergeyche@gmail.com>
2026-09-17 21:46:26 +0300

    Update main.rs
    Update menu

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,25 @@
-use std::io;
+use std::io::{self, Write};
 use std::process::Command;
 
 fn main() {
-  let mut number = 0;
-  const buttons[3]: [&str, &str, &str] = {
-    "File convert",
-    "Text convert",
-    "Exit"
-  };
-
+  let mut data = String::new();
   loop {
     Command::new("clear").status().unwrap();
     println!("markdown-to-html\n");
+    println!("1. File convert\n2. Text convert\n3. Exit");
 
-    for idx in 0..buttons.len() {
-      println!("{} {buttons[idx]}", if idx == number ">>" else ">");
+    print!("[1, 2 or 3] >");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut data).expect("Failed to read line");
+
+    match data.trim() {
+      "1" => file_convert(),
+      "2" => text_convert(),
+      "3" => break,
+      _ => {}
     }
   }
 }
+
+fn file_convert() {}
+fn text_convert() {}
