1ccd69d61e58d4c034f87a9f190e8f57620f2cbc
Ice748 <vinogradovsergeyche@gmail.com>
2026-09-17 18:46:01 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::io;
+use std::process::Command;
+
 fn main() {
-  println!("Hello World!");
+  let mut number = 0;
+  const buttons[3]: [&str, &str, &str] = {
+    "File convert",
+    "Text convert",
+    "Exit"
+  };
+
+  loop {
+    Command::new("clear").status().unwrap();
+    println!("markdown-to-html\n");
+
+    for idx in 0..buttons.len() {
+      println!("{} {buttons[idx]}", if idx == number ">>" else ">");
+    }
+  }
 }
