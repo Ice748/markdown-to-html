451c68e3ecb1df4f577ba00ca94530f4e2fea94f
Ice748 <vinogradovsergeyche@gmail.com>
2026-09-18 13:46:19 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 use std::io::{self, Write};
-use std::fs;
+use std::fs::{self, File};
 use std::process::Command;
+
+fn clear() {
+  if cfg!(target_os = "windows") {
+    Command::new("cls").status().unwrap();
+  } else {
+    Command::new("clear").status().unwrap();
+  }
+}
 
 fn main() {
   let mut data = String::new();
   loop {
-    Command::new("clear").status().unwrap();
+    clear();
     println!("markdown-to-html\n");
     println!("1. File convert\n2. Text convert\n3. Exit");
 
@@ -19,68 +27,70 @@ fn main() {
       "3" => break,
       _ => {}
     }
+    data.clear();
   }
 }
 
 fn file_convert() {
   let mut data = String::new();
 
+  clear();
   println!("File convert");
 
   print!("Enter the .md file path: ");
   io::stdout().flush().unwrap();
-  io::stdin().read_line(&mut data).expect("Failed to reaad line");
+  io::stdin().read_line(&mut data).expect("Failed to read line");
 
-  data.trim();
-  let contents = fs::read_to_string(data).expect("Failed to read file");
-  let mut file = File::create(format!("{data}.html")).expect("Failed to create file");
+  let path = data.trim();
+  let contents = fs::read_to_string(path).expect("Failed to read file");
+  let mut file = File::create(format!("{path}.html")).expect("Failed to create file");
 
   let mut body = String::new();
   let mut chars_iter = contents.chars().peekable();
 
   let mut is_bold = false;
-  let mut is_itlalics = false;
+  let mut is_italics = false;
 
   while let Some(c) = chars_iter.next() {
       if c == '*' && chars_iter.peek() == Some(&'*') {
           chars_iter.next();
           
           if is_bold {
-              html_body.push_str("</b>");
+              body.push_str("</b>");
               is_bold = false;
           } else {
-              html_body.push_str("<b>");
+              body.push_str("<b>");
               is_bold = true;
           }
       } else if c == '*' {
           if is_italics {
-              html_body.push_str("</i>");
+              body.push_str("</i>");
               is_italics = false;
           } else {
-              html_body.push_str("<i>");
+              body.push_str("<i>");
               is_italics = true;
           }
       } else {
-          html_body.push(c);
+          body.push(c);
       }
   }
 
   let html = format!(
     r#"<!DOCTYPE html>
-    <html lang="en">
-    <head>
-    	<meta charset="UTF-8">
-    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-    	<title>{data}</title>
-    </head>
-    <body>
-      {contents}
-    </body>
-    </html>
-    "#
-  )
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>{path}</title>
+</head>
+<body>
+  {body}
+</body>
+</html>
+"#
+  );
   file.write_all(html.as_bytes()).expect("Failed to write to file");
-  println!("Succefully converted to {data}.html!");
+  println!("Successfully converted to {path}.html!");
 }
 
 fn text_convert() {}
