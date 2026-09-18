4b3d82b2be815cb011e18c93cbe6393579a21a05
Ice748 <vinogradovsergeyche@gmail.com>
2026-09-18 11:25:23 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io::{self, Write};
+use std::fs;
 use std::process::Command;
 
 fn main() {
@@ -21,5 +22,65 @@ fn main() {
   }
 }
 
-fn file_convert() {}
+fn file_convert() {
+  let mut data = String::new();
+
+  println!("File convert");
+
+  print!("Enter the .md file path: ");
+  io::stdout().flush().unwrap();
+  io::stdin().read_line(&mut data).expect("Failed to reaad line");
+
+  data.trim();
+  let contents = fs::read_to_string(data).expect("Failed to read file");
+  let mut file = File::create(format!("{data}.html")).expect("Failed to create file");
+
+  let mut body = String::new();
+  let mut chars_iter = contents.chars().peekable();
+
+  let mut is_bold = false;
+  let mut is_itlalics = false;
+
+  while let Some(c) = chars_iter.next() {
+      if c == '*' && chars_iter.peek() == Some(&'*') {
+          chars_iter.next();
+          
+          if is_bold {
+              html_body.push_str("</b>");
+              is_bold = false;
+          } else {
+              html_body.push_str("<b>");
+              is_bold = true;
+          }
+      } else if c == '*' {
+          if is_italics {
+              html_body.push_str("</i>");
+              is_italics = false;
+          } else {
+              html_body.push_str("<i>");
+              is_italics = true;
+          }
+      } else {
+          html_body.push(c);
+      }
+  }
+
+  let html = format!(
+    r#"<!DOCTYPE html>
+    <html lang="en">
+    <head>
+    	<meta charset="UTF-8">
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    	<title>{data}</title>
+    </head>
+    <body>
+      {contents}
+    </body>
+    </html>
+    "#
+  )
+  file.write_all(html.as_bytes()).expect("Failed to write to file");
+  println!("Succefully converted to {data}.html!");
+}
+
 fn text_convert() {}
